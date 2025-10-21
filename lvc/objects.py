be6e476bd72f2bdc68520fbc548427d6188ddbe1
Muhammed Rhasil <muhammed.rhasil7@gmail.com>
2025-10-21 22:53:45 +0530

    Minor bug  fix

--- lvc/objects.py
+++ lvc/objects.py
@@ -1,13 +1,18 @@
-# class GitObject:
-#     def __init__(self, type: str, content: bytes):
-#         self.type = type
-#         self.content = content
-    
-#     def hash(self):
-#         header = f"{self.type} {len(self.content)}\0".encode()
-#         data = header + self.content
-#         return hashlib
 import hashlib
-data = "Hello world".encode()
-hashed = hashlib.sha1(data)
-print(hashed.hexdigest())
+import zlib
+class GitObject:
+    def __init__(self, type: str, content: bytes):
+        self.type = type
+        self.content = content
+    
+    def hash(self):
+        header = f"{self.type} {len(self.content)}\0".encode()
+        data = header + self.content
+        return hashlib.sha1(data).hexdigest()
+    
+    def serialize(self):
+        header = f"{self.type} {len(self.content)}\0".encode()
+        data = header + self.content
+        return zlib.compress(data)
+
+
