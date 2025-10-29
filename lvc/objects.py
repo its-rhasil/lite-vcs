98a85ebe0db8c2885752a93e54120ed53736b922
Muhammed Rhasil <muhammed.rhasil7@gmail.com>
2025-10-29 23:49:43 +0530

    Add git add functionality for file

--- lvc/objects.py
+++ lvc/objects.py
@@ -1,18 +1,25 @@
 import hashlib
+from __future__ import annotations
 import zlib
 class GitObject:
     def __init__(self, type: str, content: bytes):
         self.type = type
         self.content = content
     
-    def hash(self):
+    def hash(self)->str:
         header = f"{self.type} {len(self.content)}\0".encode()
         data = header + self.content
         return hashlib.sha1(data).hexdigest()
     
-    def serialize(self):
+    def serialize(self)->bytes:
         header = f"{self.type} {len(self.content)}\0".encode()
         data = header + self.content
         return zlib.compress(data)
-
-
+    @classmethod
+    def deserialize(cls, data)->GitObject:
+        decompressed = zlib.decompress(data)
+        null_idx = decompressed.find(b"\0")
+        header = decompressed[:null_idx]
+        content = decompressed[null_idx+1:]
+        obj_type, size = header.split(" ") 
+        return cls(obj_type, content)
--- lvc/repository.py
+++ lvc/repository.py
@@ -1,8 +1,11 @@
 from pathlib import Path
+from blob import Blob
+from objects import GitObject
 import json
+from typing import Dict 
 
 class Repository: 
-    def __init__(self, path="."):
+    def __init__(self, path : Path ="."):
         self.path = Path(path).resolve()
         #path for creating directories
 
@@ -27,4 +30,45 @@ class Repository:
         self.index_file.write_text(json.dumps({},indent=2))
 
         return True
-    
+    def load_index(self)->Dict[str, str]:
+        if not self.index_file.exists():
+            self.index_file.write_text(json.dumps({},indent=2))
+            return {}
+        try:
+            return json.loads(self.index_file.read_text())
+        except:
+            return {}
+        
+    def store_object(self, obj:GitObject)-> str:
+        obj_hash = obj.hash()
+        obj_dir = self.objects_dir / obj_hash[:2]
+        obj_file = obj_dir / obj_hash[2:]
+
+        if not obj_file.exists():
+            obj_dir.mkdir(exist_ok=True)
+            obj_file.write_bytes(obj.serialize())
+
+        return obj_hash
+    def save_index(self,index: Dict[str,str]):
+        self.index_file.write_text(json.dumps(index,indent=2))
+    def add_file(self,file_path:Path):
+        content = file_path.read_bytes()
+        blob = Blob(content)
+        hashed = self.store_object(blob)
+        index = self.load_index()
+        index[file_path.name] = hashed
+        self.save_index(index)
+        print(f"Added {file_path.name} to the staging area")
+
+
+
+    def add_path(self, path: str)->None:
+        absolute_path = self.path / path
+        if not absolute_path.exists():
+            raise FileNotFoundError(f"Path {absolute_path} not found!")
+        if absolute_path.is_file():
+            self.add_file(absolute_path)
+        elif absolute_path.is_dir():
+            self.add_dir(absolute_path)
+        else:
+            raise ValueError("Neither a File nor a directory")
